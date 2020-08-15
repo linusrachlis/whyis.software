711dff3452cbf001cbb006b538a7845ccfc9f32a
Linus Rachlis <linus@rachlis.net>
2020-08-15 11:48:32 -0400

    Stop versioning dist files; concat css

--- scripts/build.php
+++ scripts/build.php
@@ -1,18 +1,34 @@
 <?php
 
-$list = [
+$pages = [
     'index',
     'cookies',
+];
+
+$css_files = [
+    'normalize',
+    'main',
+    'custom',
 ];
 
 $src_dir = __DIR__ . '/../src';
 $dist_dir = __DIR__ . '/../dist';
 
+if (!is_dir($dist_dir)) {
+    mkdir($dist_dir);
+}
+
 require $src_dir . '/_inc.php';
 
-foreach ($list as $page) {
+foreach ($pages as $page) {
     ob_start();
     require $src_dir . '/' . $page . '.php';
     $output = ob_get_clean();
     file_put_contents($dist_dir . '/' . $page . '.html', $output);
 }
+
+$css_code = '';
+foreach ($css_files as $css_file) {
+    $css_code .= file_get_contents("$src_dir/css/$css_file.css") . PHP_EOL;
+}
+file_put_contents("$dist_dir/style.css", $css_code);
