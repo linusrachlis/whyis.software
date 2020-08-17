3bd002d2829cba0fcefa930844fd65892c00126c
Linus Rachlis <linus@rachlis.net>
2020-08-16 20:50:51 -0400

    Revise about section

--- src/index.php
+++ src/index.php
@@ -17,7 +17,8 @@
             <li><em>Coming soon:</em> Password strength, two-factor authentication, not re-using passwords. Why do these matter?</li>
             <li><em>Coming soon:</em> I upgraded to Mac OS Catalina and can't use 32-bit apps anymore. What does that mean?</li>
             <li><em>Coming soon:</em> Computers today are 1000 times as powerful as early PCs were, right? Why do they often still feel slow?</li>
-            <li><a href="mailto:ask@whyis.software">Email a question</a></li>
+            <li><a href="mailto:ask@whyis.software">Email a question</a>. If you're afraid it may be a stupid question,
+                then I really want to hear it!</li>
         </ul>
     </section>
 
@@ -25,27 +26,25 @@
         <h1>About</h1>
         <?php T::markdown_begin() ?>
 
-When I first used a computer, the experience was stuffy and arcane, but
-awe-inspiring. The thing waited for you to figure it out -- or not.
-Since then, computing has gone mainstream. A few companies have grown
-massive by making computing into a sleek and magical experience --
-except that we grow bored of the magic, and distrustful of the
-never-ending hiccups. It seems an unhealthy relationship.
+When I first used a computer, I felt uncertainty and awe. This was an
+information powertool, and I had better know what I was doing. Since
+then, a few companies have grown massive by redefining personal
+computing as a sleek and magical experience. Users are no longer
+supposed to understand how software works.
 
-I made this site because even if software isn't your field, it's a big
-part of your life. You probably find it useful, curious, maddening,
-confounding, addicting, and maybe sometimes still awe-inspiring. Let me
-tell you from the inside: there's nothing magical or sleek about
-software. There's just a lot of people working hard, making a mess, and
-trying to keep it from getting too out of hand. It all affects your
-life, so you deserve more insight.
+On the one hand, this lowers computing's barrier to entry. It's good
+that more people can benefit from shared digital information.
 
-Despite all that effort to make the sailing feel smooth, the icebergs of
-complexity often poke up above the surface, hinting at the unimaginable
-depth. Users are expected to cope and adapt, but not to question. I've
-based each article on the tip of such an iceberg, providing a high-level
-answer, followed by additional questions you can expand, depending where
-you're interested in diving deeper.
+On the other hand, a tool you're not supposed to understand is
+disempowering, especially when it malfunctions, which software often
+does. Furthermore, the software industry needs more accountability, both
+for its own good and for the public good. This requires demystification.
+
+This website is for folks who don't make software, but often wonder
+about it. It's such a big part of your life, you deserve some
+explanation. For each question, I give a high-level answer followed by
+more questions you can expand, depending where you're interested in
+diving deeper.
 
 I'm a software developer in Toronto, ON, Canada. You can also find me at
 my [strange personal website](https://linus.rachlis.net),
