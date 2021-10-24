eb43eea8d24e76fd1b9f330b175344818c58d6f4
Linus Rachlis <linus@rachlis.net>
2021-10-24 16:32:02 -0400

    reboot!

--- src/_inc.php
+++ src/_inc.php
@@ -37,7 +37,9 @@ class T {
     <meta name="description" content="Opinionated answers for the non-technical tech-curious">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#000000">
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Sans+Condensed:ital,wght@0,400;0,700;1,400;1,700&family=Fira+Sans:ital,wght@0,400;0,700;1,400;1,700&family=VT323&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,600;1,400;1,600&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/style.css" class="site-styles">
 <? if (DEV_MODE) { ?>
     <script>
@@ -64,51 +66,8 @@ class T {
         <?php
     }
 
-    function article_head($article_title, array $breadcrumbs = []) {
-        ?>
-
-    <article>
-        <div class=header>
-            <div class=breadcrumbs>
-                <a href="/">Why is software?</a> &raquo;
-                <?php foreach ($breadcrumbs as $url => $title): ?>
-
-                <a href="<?= $url ?>"><?= self::typograph($title) ?></a> &raquo;
-                <?php endforeach ?>
-            </div>
-            <h1><?= self::typograph($article_title) ?></h1>
-        </div>
-        <div class=content>
-        <?php
-    }
-
-    function article_foot(array $backlinks = []) {
-        ?>
-
-            </div>
-        </article>
-
-        <footer>
-            <?php foreach ($backlinks as $url => $title): ?>
-
-            <p>&lt; <a href="<?= $url ?>"><?= self::typograph($title) ?></a></p>
-            <?php endforeach ?>
-            <p>&lt; <a href="/">Return home</a></p>
-        </footer>
-        <?php
-    }
-
     function foot() {
         ?>
-
-    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-    <!--
-    <script>
-        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-        ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-    </script>
-    <script src="https://www.google-analytics.com/analytics.js" async></script>
-    -->
     </body>
 
 </html>
--- src/index.php
+++ src/index.php
@@ -3,51 +3,49 @@
     <header>
         <h1>Why is software?</h1>
         <p class=tagline>
-            Opinionated answers for the non-technical tech-curious.<br>
-            How the sausage is programmed.
+            <script>
+            (function() {
+                var taglines = [
+                    'Opinionated answers for the non-technical tech-curious.',
+                    'How the sausage is programmed.',
+                ];
+                var choice = Math.round(Math.random() * (taglines.length-1));
+                document.write(taglines[choice]);
+            })();
+            </script>
         </p>
     </header>
 
-    <section>
-        <h2>Articles</h2>
-
-        <ul class=articles>
-            <li><a href="/cookies/">What are cookies, and why does every website make me agree to them?</a></li>
-            <li><em>Coming soon:</em> Why do I use some apps through a web browser and others have to be installed?</li>
-            <li><em>Coming soon:</em> Password strength, two-factor authentication, not re-using passwords. Why do these matter?</li>
-            <li><em>Coming soon:</em> I upgraded to Mac OS Catalina and can't use 32-bit apps anymore. What does that mean?</li>
-            <li><em>Coming soon:</em> Computers today are 1000 times as powerful as early PCs were, right? Why do they often still feel slow?</li>
-            <li><a href="mailto:ask@whyis.software">Email a question</a>. If you're afraid it may be a stupid question,
-                then I really want to hear it!</li>
-        </ul>
+    <section class=article-previews>
+        <a href="/facebook-and-the-internet/">
+            The 2021 Facebook outage: what happened, and what is the Internet
+            anyway?
+        </a>
+        <a href="/foo-bar-zam/">
+            Foo bar, and also zam
+        </a>
     </section>
 
-    <section>
+    <section class=body-text>
         <h2>Why is this website?</h2>
         <?php T::markdown_begin() ?>
 
 When I first used a computer, I felt uncertainty and awe. This was an
-information powertool, and I had better know what I was doing. Since
-then, a few companies have grown massive by redefining personal
-computing as a sleek and magical experience. Users are no longer
-supposed to understand how software works.
+information powertool, and I had better know what I was doing. Since then, a
+few companies have grown massive by redefining personal computing as a sleek
+and magical experience. Users are no longer supposed to understand how software
+works.
 
-On the one hand, this lowers computing's barrier to entry. It's good
-that more people can benefit from shared digital information.
+On the one hand, this lowers computing's barrier to entry. It's good that more
+people can benefit from shared digital information. On the other hand, using a
+tool you're not supposed to understand is undignified and disempowering,
+especially when it malfunctions, which software often does.
 
-On the other hand, a tool you're not supposed to understand is
-disempowering, especially when it malfunctions, which software often
-does. Furthermore, the software industry needs more accountability, both
-for its own good and for the public good. This requires demystification.
-
-This website is for folks who don't make software, but often wonder
-about it. It's such a big part of your life, you deserve some
-explanation. For each question, I give a high-level answer followed by
-sub-questions you can visit, depending where you're interested in
-diving deeper.
+This website is for folks who don't make software, but often wonder about it.
+It's such a big part of your life, you deserve some explanation.
 
 I'm a software developer in Toronto, ON, Canada. I also write on other topics
-on my [personal website](https://linus.rachlis.net) and share some on
+on my [personal website](https://linus.rachlis.net) and share some code on
 [Github](https://github.com/linusrachlis).
 
         <?php T::markdown_end() ?>
